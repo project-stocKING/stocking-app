edea311e3438ac1431dc8329ef5c7ce5eae126cb
Aleksander Cieslak <olek.cieslak@yahoo.com>
2016-03-20 21:02:09 +0100

    Smooth scrolling bug fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,27 +19,23 @@
 //= require components
 //= require_tree .
 
-$(document).ready(function(){
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+$(document).on('click', ".navbar a:not('#logo'), footer a[href='#myPage']", function(event) {
+    // Prevent default anchor click behavior
+    event.preventDefault();
 
-  // Prevent default anchor click behavior
-  //event.preventDefault();
+    // Store hash
+    var hash = this.hash;
 
-  // Store hash
-  var hash = this.hash;
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
 
-  // Using jQuery's animate() method to add smooth page scroll
-  // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-  $('html, body').animate({
-    scrollTop: $(hash).offset().top
-  }, 900, function(){
-
-    // Add hash (#) to URL when done scrolling (default click behavior)
-    window.location.hash = hash;
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
     });
-  });
-})
+});
 
 
 $(window).scroll(function() {
